c3c9a0e086c284c34006351d7ae286034198edf3
rathor-kartik <rathoransh01@gmail.com>
2025-03-24 19:45:07 +0530

    Your commit message

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,4 +2,5 @@ import mongoose from 'mongoose'
 
 export  const connectDB =async ()=>{
     await mongoose.connect("mongodb+srv://foodpro:whitesaucepasta@cluster0.vl0cc.mongodb.net/food-del").then(()=>console.log("DB Connected"));
+    // await mongoose.connect("mongodb+srv://foodpro:whitesaucepasta@cluster0.vl0cc.mongodb.net/food-del").then(()=>console.log("DB Connected"));
 }
